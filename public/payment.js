95f96aa76329e57f2f883d6936ae0884dc4114a5
Harshit Thakkar <harshitnth@gmail.com>
2021-02-18 20:03:42 +0530

    UI changes for payment

--- public/payment.js
+++ public/payment.js
@@ -280,3 +280,26 @@ securitycode.addEventListener('focus', function () {
     document.querySelector('.creditcard').classList.add('flipped');
 });
 };
+paypal.Buttons({
+
+    // Set up the transaction
+    createOrder: function (data, actions) {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '88.44'
+          }
+        }]
+      });
+    },
+
+    // Finalize the transaction
+    onApprove: function (data, actions) {
+      return actions.order.capture().then(function (details) {
+        // Show a success message to the buyer
+        alert('Transaction completed by ' + details.payer.name.given_name + '!');
+      });
+    }
+
+
+  }).render('#paypal-button-container');
