82813bb10281562a08444ab34c3fc4a05327cdd9
Harshit Thakkar <harshitnth@gmail.com>
2021-02-25 14:08:06 +0530

    home page ui changes

--- config/payment_details_insert.js
+++ config/payment_details_insert.js
@@ -2,7 +2,7 @@ const con = require("../config/database");
 
 module.exports.addpaymentdetails=(name,cardnumber,expiration_date,securitycode,cb)=>{
     console.log("Database"+cardnumber);
-    con.query("create table if not exists payments(name varchar(20),cardnumber int(17),expiration_date varchar(6),securitycode int(4));");
+    con.query("create table if not exists payments(name varchar(20),cardnumber varchar(17),expiration_date varchar(6),securitycode int(4));");
     sql=`INSERT INTO payments VALUES(?,?,?,?)`;
     con.query(sql,[name,cardnumber,expiration_date,securitycode],(err,result)=>{
         if(err){
