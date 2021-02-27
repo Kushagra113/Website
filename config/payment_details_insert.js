1eef47bb9357ce1078168c1076182538083b7ac9
Kushagra113 <tibrewalkushagra@gmail.com>
2021-02-27 15:21:15 +0530

    Added Amount Input text to Payments page

--- config/payment_details_insert.js
+++ config/payment_details_insert.js
@@ -1,10 +1,9 @@
 const con = require("../config/database");
 
-module.exports.addpaymentdetails=(name,cardnumber,expiration_date,securitycode,cb)=>{
-    console.log("Database"+cardnumber);
-    con.query("create table if not exists payments(name varchar(20),cardnumber varchar(17),expiration_date varchar(6),securitycode int(4));");
-    sql=`INSERT INTO payments VALUES(?,?,?,?)`;
-    con.query(sql,[name,cardnumber,expiration_date,securitycode],(err,result)=>{
+module.exports.addpaymentdetails=(name,cardnumber,expiration_date,securitycode,amount,cb)=>{
+    con.query("create table if not exists payments(name varchar(20),cardnumber varchar(17),expiration_date varchar(6),securitycode int(4),amount int(8),date DATETIME DEFAULT CURRENT_TIMESTAMP);");
+    sql=`INSERT INTO payments(name,cardnumber,expiration_date,securitycode,amount) VALUES(?,?,?,?,?)`;
+    con.query(sql,[name,cardnumber,expiration_date,securitycode,amount],(err,result)=>{
         if(err){
             console.log(err);
             cb(err,null);
@@ -14,3 +13,19 @@ module.exports.addpaymentdetails=(name,cardnumber,expiration_date,securitycode,c
         }
     })
 }
+
+module.exports.test_date=(username,cb)=>{
+    sql=`select date from payments where name=?`;
+    con.query(sql,username,(err,result)=>{
+        if(err){
+            console.log(err);
+            cb(err,null);
+        }
+        else{
+            console.log("Date"+result);
+            console.log("Date"+result[0].date);
+            console.log(result[0].date.getMonth());            
+        }
+    })
+}
+
--- routes/admin.js
+++ routes/admin.js
@@ -9,59 +9,58 @@ const complaints = require("../config/crud");
 const auth_function = require("../config/auth");
 
 // Admin Home Page
-router.get("/home",auth_function.ensureAdmin ,(req, res) => {
-    res.render("home",{
-        user:req.user
+router.get("/home", auth_function.ensureAdmin, (req, res) => {
+    res.render("home", {
+        user: req.user
     });
 })
 
 // Admin Complaint Page
-router.get("/complaint",auth_function.ensureAdmin,(req,res)=>{
-    res.render("complaint",{
-        user:req.user
+router.get("/complaint", auth_function.ensureAdmin, (req, res) => {
+    res.render("complaint", {
+        user: req.user
     })
 });
 
 // Admin Payment Page
-router.get("/payment", auth_function.ensureAdmin,(req, res) => {
-    res.render("payment",{
-        user:req.user
+router.get("/payment", auth_function.ensureAdmin, (req, res) => {
+    res.render("payment", {
+        user: req.user
     });
 })
 
 // Admin About Us Page
-router.get("/aboutus",auth_function.ensureAdmin, (req,res)=>{
-    res.render("aboutus",{
-        user:req.user
+router.get("/aboutus", auth_function.ensureAdmin, (req, res) => {
+    res.render("aboutus", {
+        user: req.user
     });
 })
 
 // Adding Payment Details to the database
-router.post("/payment",auth_function.ensureAuthenticated,(req,res)=>{
-    const {name,cardnumber,expirationdate,securitycode} = req.body;
-    console.log(cardnumber);
-    payments.addpaymentdetails(name,cardnumber,expirationdate,securitycode,(err,result)=>{
-        if(err){
-            res.json({msg:"error"})
+router.post("/payment", auth_function.ensureAuthenticated, (req, res) => {
+    const { name, cardnumber, expirationdate, securitycode, amount } = req.body;
+    payments.addpaymentdetails(name, cardnumber, expirationdate, securitycode, amount, (err, result) => {
+        if (err) {
+            res.json({ msg: "error" })
         }
-        else{
-            res.json({msg:"success_insert"});
+        else {
+            res.json({ msg: "success_insert" });
         }
     });
 });
 
 
 // Admin Deleting Permissions
-router.delete("/complaint/removecomplaint",auth_function.ensureAdmin,(req,res)=>{
-    complaints.removecomplaint(req.body.id,req.body.complaint,(err,result)=>{
-        if(err){
+router.delete("/complaint/removecomplaint", auth_function.ensureAdmin, (req, res) => {
+    complaints.removecomplaint(req.body.id, req.body.complaint, (err, result) => {
+        if (err) {
             console.log(err);
-            res.json({msg:"error"});
+            res.json({ msg: "error" });
         }
-        else{
-            res.json({msg:"success"});
+        else {
+            res.json({ msg: "success" });
         }
     });
 })
 
-module.exports=router;
+module.exports = router;
