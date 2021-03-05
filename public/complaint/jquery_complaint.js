1a74af2d5b4dfa7ccab881aeab371e843e659124
Kushagra113 <tibrewalkushagra@gmail.com>
2021-03-05 19:42:22 +0530

    UI and Bug Fix for Complaint Page

--- public/complaint/jquery_complaint.js
+++ public/complaint/jquery_complaint.js
@@ -8,28 +8,33 @@ $(document).ready(function(){
         // var name=$("#name").val();
         var c = $("#complaint_text").val();
         // alert(name);
-        $.ajax({
-            URL:'/users/complaint',
-            method: 'POST',
-            dataType:'json',
-            data: {'complaint':c},
-            success:function(response){
-                if(response.msg=='success'){
-                    // alert("Name Added In the Database");
-                    getData();
+        if(c===""){
+            alert("Enter Some Complaint to Add it");
+        }
+        else{
+            $.ajax({
+                URL:'/users/complaint',
+                method: 'POST',
+                dataType:'json',
+                data: {'complaint':c},
+                success:function(response){
+                    if(response.msg=='success'){
+                        // alert("Name Added In the Database");
+                        getData();
+                    }
+                    else if(response.msg=="same_complaint"){
+                        alert("Complaint Already Added")
+                    }
+                    else{
+                        alert("Some Error Occured Try Again");
+                    }
+                },
+                error:function(){
+                    // console.log(error);
+                    alert('server error occured');  
                 }
-                else if(response.msg=="same_complaint"){
-                    alert("Complaint Already Added")
-                }
-                else{
-                    alert("Some Error Occured Try Again");
-                }
-            },
-            error:function(){
-                // console.log(error);
-                alert('server error occured');  
-            }
-        });
+            });
+        }
     });
     $(document).on('click','button.del',function(){  
         var id = $(this).parent().find('button.del').val();
@@ -101,16 +106,19 @@ $(document).ready(function(){
         })
     }); 
     $(document).on('click','button.edit',function(e){
-        var id = $(this).parent().find('button.edit').val();
-        // if(result==1){
-               
-        // }
-        // else{
+        if($(".edit_block").is(":visible")){
+            alert("First Edit the Following Complaint");
+        }
+        else{
+            var id = $(this).parent().find('button.edit').val();
             complaint=($(this).parent().parent().find(".complaint_text").html());
             $(".edit_block").show();
             $(document).one("click","#done",function(){
                 var new_complaint = $(document).find("#edit_complaint").val();
-                console.log("New Complaint"+new_complaint);
+                if(new_complaint===undefined){
+                    alert("Please Enter Complaint to Edit or Reload the page");
+                }
+                else{
                     $.ajax({  
                             url:'/users/complaint/editcomplaint',  
                             method:'post',  
@@ -137,7 +145,9 @@ $(document).ready(function(){
                             }  
                         });  
                         $(".edit_block").hide();
+                }
             });
+        }
         // }
     }); 
     function getData(){
@@ -147,7 +157,6 @@ $(document).ready(function(){
             dataType:'json',
             success:function(response){
                 if(response.msg=="success"){
-                    alert("Hello")
                     $.ajax({
                         url:'/admin/complaint/getdata',
                         method:'GET',
@@ -211,26 +220,7 @@ $(document).ready(function(){
             }
         })
     }
-    // function checkAdmin(){
-    //     var result;
-    //     $.ajax({
-    //         url:'/checkadmin',
-    //         method: 'get',
-    //         dataType:'json',
-    //         success:function(response){
-    //             if(response.msg=="success"){
-    //                 result=1;
-                    
-    //             }
-    //             else if(response.msg=="user_error"){
-    //                 result=0;
-    //             }
-    //             else{
-    //                 alert("server Error Occured");
-    //             }
-    //         }
-    //     })
-    // }
-    
-    
+    $("#hide_edit").click(function(){
+        $(document).find(".edit_block").hide();
+    })
 });
